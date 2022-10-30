961a0762af5ffc3bd6295afaf0d6320cbc78c513
Jake Schmidt <jakeschmidt51@gmail.com>
2022-10-30 15:38:27 -0700

    created html

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const option1 = document.querySelector('#option1')
+const option2 = document.querySelector('#option2')
+const option3 = document.querySelector('#option3')
+const option4 = document.querySelector('#option4')
